3d94cef487e82ba4cc2a966fd1497fabd3235cab
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2017-11-26 10:45:19 +0100

    first sketch of player

--- vue/js/main.js
+++ vue/js/main.js
@@ -3,15 +3,31 @@ new Vue({
     data: {
         oeuvre: {
             title: 'test'
-        }
+        },
+        player: {
+            cover: null,
+            playlist: [],
+        },
     },
     created() {
 
         axios
         .get('http://localhost:3000/work/13920002')
         .then(response => {
-            this.oeuvre = response.data
-       
+            this.oeuvre = response.data;
+            if (response.data.eans && response.data.eans.length) {
+                fetch(`http://localhost:3000/deezer/${response.data.eans.join(',')}`)
+                    .then(response => response.json())
+                    .then(response => {
+                        for (const album of response) {
+                            if (album.id && album.tracks && album.tracks.length) {
+                                this.player.cover = album.cover;
+                                this.player.playlist = album.tracks;
+                                break;
+                            }
+                        }
+                })
+            }
         })
         // .catch(e => () {
 
